48659036db1774334ef8b98a07e1d9764e95c299
Chetan Kamble <cckamble2004@gmail.com>
2025-07-31 13:37:44 +0530

    third updates

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectdb from "./db/dbconnect.js"
 import userrouter from "./router/userroutes.js";
 import jobrouter from './router/jobsroutes.js';
+import cors from "cors";
 
 const app = express();
 dotenv.config();//load env variables
@@ -11,6 +12,7 @@ connectdb();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.status(200).send("server is running fine");
